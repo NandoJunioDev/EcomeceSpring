4a9508312f4f43338e9d0fe1a4f03c116827722e
NandoJunioDev <Frnndmaz_cf@outlook.com>
2025-03-05 09:36:28 -0300

    Inicio do Projeto 05/03

---
+++ ecomece/src/main/java/ecomece/ecomece/EcomeceApplication.java
@@ -0,0 +1,13 @@
+package ecomece.ecomece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomeceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomeceApplication.class, args);
+	}
+
+}
---
+++ ecomece/src/test/java/ecomece/ecomece/EcomeceApplicationTests.java
@@ -0,0 +1,13 @@
+package ecomece.ecomece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomeceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
