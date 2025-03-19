eb31dee06a6cbbc8df99eaf53088166cd0c380e9
NandoJunioDev <frnndmaz_cf@outlook.com>
2025-03-18 23:28:06 -0300

    18/03

--- ecomece/src/main/resources/static/js/animaçao.js
+++ ecomece/src/main/resources/static/js/animaçao.js
@@ -4,6 +4,6 @@ window.onload = function() {
         // Exibe a mensagem por 3 segundos e depois a esconde suavemente
         setTimeout(() => {
             mensagemErro.style.opacity = '0';  // Reduz a opacidade para desaparecer
-        }, 3000); // Tempo em milissegundos (3 segundos)
+        }, 1000); // Tempo em milissegundos (3 segundos)
     }
 };
