fb9be4f3c62ae2774a218c4dadc3287b57c90504
NandoJunioDev <Frnndmaz_cf@outlook.com>
2025-03-27 14:03:50 -0300

    27/0/2025

--- ecomece/src/main/java/ecomece/ecomece/Repository/UsuarioRepository.java
+++ ecomece/src/main/java/ecomece/ecomece/Repository/UsuarioRepository.java
@@ -6,14 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ecomece.ecomece.Cadastro.Cadastros;
-import java.util.List;
 
 
 @Repository
 
 public interface UsuarioRepository  extends  JpaRepository  <Cadastros, Long> {
-        Optional <Cadastros>  findByEmail(String email);
-        boolean existsByEmail(String email);
+        Optional <Cadastros>  findByEmail(String email);  // busca p usuario pelo email
+        boolean existsByEmail(String email); //verifica de email ja existe
         
 }
 
